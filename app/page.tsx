5e12cad4bacb98492a9290b9177f13a2fc874ea7
Marcela Silva <marcela013sb@hotmail.com>
2025-04-19 09:46:26 -0300

    [feat] Estilização geral dos icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import AboutMe from "@/components/AboutMe/page"
+import CodeIcon from '@mui/icons-material/Code';
 
 export default function Home() {
   return (
     <div>
+      
       <AboutMe />
+
     </div>
   );
 }
