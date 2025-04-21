24a5f4902ce9908c6d70ea7ca4e92429aa31e43d
Marcela Silva <marcela013sb@hotmail.com>
2025-04-21 19:15:30 -0300

    [feat] Preparos gerais de Estados

--- components/ContactMe/page.tsx
+++ components/ContactMe/page.tsx
@@ -1,19 +1,33 @@
+"use client"
 import { Button, TextField } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
+import { useState } from "react";
+
 export default function ContactMe() {
+    const [name,setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [about, setAbout] = useState("")
+    const [text, setText] = useState("")
+
+    function sendEmail(){
+        
+    }
+
     return (
             
         <div className="m-0 mt-20 sm:m-20 sm:mt-0 p-5 sm:p-10 lg:p-20 bg-white/10 rounded-2xl backdrop-blur-lg">
 
             <h1 className="md:-mt-5 mb-5 text-[30px] md:text-[40px]">Contact Me</h1>
 
-            <form action="" method="get" className="flex flex-col gap-5">
+            <form onSubmit={sendEmail} className="flex flex-col gap-5">
                 <TextField 
                     variant="outlined" 
                     name="name" 
                     label="Nome" 
                     required
                     className="rounded"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                 />
 
                 <TextField 
@@ -23,6 +37,8 @@ export default function ContactMe() {
                     type="email" 
                     required
                     className="rounded-lg"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <TextField 
@@ -30,6 +46,8 @@ export default function ContactMe() {
                     name="about"
                     label="Assunto" 
                     required
+                    value={about}
+                    onChange={(e) => setAbout(e.target.value)}
                 />
 
                 <TextField 
@@ -40,14 +58,15 @@ export default function ContactMe() {
                     minRows={4} 
                     maxRows={10} 
                     required
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
                 />
                 
                 <Button 
                     variant="contained"
-                    className="absolute bg-red"
                     color="secondary"
+                    type="submit"
                     endIcon={<SendIcon />}
-                    //onClick={null}
                 >
                     Send
                 </Button>
